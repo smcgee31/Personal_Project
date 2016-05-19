42f1a4943bac2eb82a2c2b2e004e7ac9e0675c70
smcgee31 <solodesign31@gmail.com>
2016-05-19 14:23:30 -0600

    midday push - styling, styling, and more styling... also lookin for more assets and content

--- public/js/homeCtrl.js
+++ public/js/homeCtrl.js
@@ -1,12 +1,6 @@
 angular.module('waterfallApp')
 .controller('homeCtrl', function($scope, mainSvc, $state) {
 
-    $scope.signup = function(newUser) {
-        mainSvc.signup(newUser)
-        .then(function(response) {
-          $state.go('waterfall');
-        });
-    };
 
 
 
