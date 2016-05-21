a254e455937b2332baff0e35f383a92c4df80a99
smcgee31 <solodesign31@gmail.com>
2016-05-21 12:11:44 -0600

    after hosting, found things that need fixing

--- public/js/signupCtrl.js
+++ public/js/signupCtrl.js
@@ -4,7 +4,7 @@ angular.module('waterfallApp')
     $scope.signup = function(newUser) {
         mainSvc.signup(newUser)
         .then(function(response) {
-          $state.go('waterfall');
+            $state.go('login');
         });
     };
 
